1ef029c3dc4ff7fa34af2e3e9f2adcbe39a23315
Brad Lugo <cblugo95@yahoo.com>
2016-09-13 21:53:55 -0500

    Added EnitityFramework for data objects

---
+++ src/AplusCurator/Models/DataObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplusCurator.Models
+{
+    public class DataObject
+    {
+    }
+}
