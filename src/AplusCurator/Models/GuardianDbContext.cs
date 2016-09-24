76f4c629b08310d0d503c20953929a1e826bc02d
Speed12548 <speed12548@gmail.com>
2016-09-23 19:35:28 -0500

    Added the Guardian Models and created a DBContext for them.

---
+++ src/AplusCurator/Models/GuardianDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplusCurator.Models
+{
+    public class GuardianDbContext : DbContext
+    {
+        public GuardianDbContext() : base()
+        {
+
+        }
+
+        public DbSet<Guardian> Guardians { get; set; }
+    }
+}
---
+++ src/AplusCurator/Models/GuardianModels.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace AplusCurator.Models
+{
+    public class Guardian
+    {
+        [Key]
+        public int GuardianId { get; set; }
+
+        [StringLength(127), Required]
+        public string FirstName { get; set; }
+
+        [StringLength(127), Required]
+        public string LastName { get; set; }
+
+        [StringLength(127), Required]
+        public string Address { get; set; }
+
+        [StringLength(127), Required]
+        public string Email { get; set; }
+
+        [StringLength(127), Required]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(127)]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        public int Role { get; set; }
+
+        [StringLength(127), Required]
+        public string ContactName { get; set; }
+
+        [StringLength(127), Required]
+        public string ContactNumber { get; set; }
+
+        public int Status { get; set; }
+
+        [StringLength(99999)]
+        public string Description { get; set; }
+
+        [StringLength(99999)]
+        public string SystemInfo { get; set; }
+
+    }
+}
