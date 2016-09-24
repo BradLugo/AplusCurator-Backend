d373bd55c16f522ee2d51e729ddfa31005b882a9
Speed12548 <speed12548@gmail.com>
2016-09-23 20:33:29 -0500

    Created Student Model and DbContext

---
+++ src/AplusCurator/Models/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplusCurator.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ src/AplusCurator/Models/StudentModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplusCurator.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [StringLength(127), Required]
+        public string FirstName { get; set; }
+
+        [StringLength(127), Required]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "Date"), Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public int Gender { get; set; }
+
+        [Required]
+        public int CurrentGrade { get; set; }
+
+        public int Status { get; set; }
+
+        [StringLength(99999)]
+        public string Description { get; set; }
+
+        [StringLength(99999)]
+        public string SystemInfo { get; set; }
+
+    }
+}
